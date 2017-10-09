005e9adb35d5b6f393db512fb3e1ca9daba9fa84
LemunDesigns <lemun420il@gmail.com>
2017-10-09 15:52:37 -0500

    Update chikabot.js

--- chikabot.js
+++ chikabot.js
@@ -146,26 +146,7 @@ client.on("message", async message => {
     message.channel.send("My discord community server! https://discord.gg/MmSrEEy")
   }
   if (message.content.startsWith(config.prefix + "8ball")) {
-        var sayings = ["It is certain",
-										"It is decidedly so",
-										"Without a doubt",
-										"Yes, definitely",
-										"You may rely on it",
-										"As I see it, yes",
-										"Most likely",
-										"Outlook good",
-										"Yes",
-										"Signs point to yes",
-										"Reply hazy try again",
-										"Ask again later",
-										"Better not tell you now",
-										"Cannot predict now",
-										"Concentrate and ask again",
-										"Don't count on it",
-										"My reply is no",
-										"My sources say no",
-										"Outlook not so good",
-										"Very doubtful"];
+        var sayings = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
 
             var result = Math.floor((Math.random() * sayings.length) + 0);
             message.reply(result);
