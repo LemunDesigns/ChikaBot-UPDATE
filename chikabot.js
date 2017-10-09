1f296f26b6c9a89de8981e7176d3e1dd78c78a52
LemunDesigns <lemun420il@gmail.com>
2017-10-09 09:47:26 -0500

    Hidden the token

--- chikabot.js
+++ chikabot.js
@@ -3,8 +3,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
-const config = require("./config.json");
-
 
 client.on("ready", () => {
 
@@ -146,4 +144,4 @@ client.on("message", async message => {
 
 });
 
-client.login(config.token);
+client.login(process.env.BOT_TOKEN);
