bab1be0cae98ed7e1cd192814b739f31cde6bcf6
LemunDesigns <lemun420il@gmail.com>
2017-10-10 20:07:34 +0300

    Update chikabot.js

--- chikabot.js
+++ chikabot.js
@@ -148,8 +148,8 @@ client.on("message", async message => {
   if (message.content.startsWith(config.prefix + "8ball")) {
         var sayings = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
 
-            var rand = sayings[Math.floor(Math.random() * sayings.length)];
-message.reply(result);
+            var result = Math.floor((Math.random() * sayings.length) + 0);
+            message.reply(sayings[result])
     }
       
 
